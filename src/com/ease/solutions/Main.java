3efe17a70efb3e642b139ad4730e4aefb87bdb8f
Karlo Chua <karlochua94@gmail.com>
2017-05-23 20:46:01 +0800

    Added File Reader Class

--- src/com/ease/solutions/Main.java
+++ src/com/ease/solutions/Main.java
@@ -1,5 +1,12 @@
 package com.ease.solutions;
 
+import com.ease.solutions.utils.FileUtils;
+
 public class Main {
+
+    public static void main(String[] args) {
+        FileUtils map = new FileUtils("map.txt");
+        System.out.println(map.toString());
+    }
 
 }
